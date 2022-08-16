442f90447706be748109c35e7f0f61478362e904
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-08-16 01:45:30 -0700

    added more inquirer prompts and polished the readme framework

--- Develop/index.js
+++ Develop/index.js
@@ -2,39 +2,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const createReadme = ({ projectTitle, description, languages }) =>
+const createReadme = ({ projectTitle, subtitle, description, area1, area2, area3, area4, area5, appLink, screenshot, screenshotDescription }) =>
 //readme skeleton here 
 `
 # ${projectTitle}
 
-## 02 Advanced CSS: Portfolio
+## ${subtitle}
 
 ${description}
 
-## ${languages}:
+## Areas Covered:
 
-* Webpage header with heading text.
-* Navigation bar with navigation buttons that shade when hovered and jump to respective sections when pressed.
-* Introductory biography for prospective employers to learn more about myself, as well as my capabilities as a developer.
-* Thumbnail clickable previews of previous projects were created to showcase past work and abilities
-* CSS elements were created in order for previous work thumbnails to stack for viewing on narrower screens.
-* Contact methods were added.
-* Used semantic HTML elements.
-* Created comments throughout the CSS style sheet to make future edits and/or navigation easier. 
+* ${area1}
+* ${area2}
+* ${area3}
+* ${area4}
+* ${area5}
 
-## Deployed application: (https://kentshaffer.github.io/module-2-challenge/)
+## Deployed application: (${appLink})
 
-![image screenshot of navigation bar and biography section of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-1.png)
+![${screenshotDescription}](${screenshot})
 
-![image screenshot of project card section of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-2.png)
-
-![image screenshot of contact section and footer of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-3.png) 
-
-![image screenshot of header, nav, and bio of deployed application on smartphone size](/Assets/images/module-2-challenge-mobile-1.png) 
-
-![image screenshot of project card section of deployed application on smartphone size](/Assets/images/module-2-challenge-mobile-2.png) 
-
-![image screenshot of header, bio and top of project card section of deployed application on tablet size](/Assets/images/module-2-challenge-tablet-1.png) 
 `;
 
 inquirer
@@ -56,8 +44,28 @@ inquirer
     },
     {
       type: 'input',
-      message: 'What languages did you use?',
-      name: 'languages',
+      message: '1/5 what is one area covered in your application?',
+      name: 'area1',
+    },
+    {
+      type: 'input',
+      message: '2/5 what is one area covered in your application?',
+      name: 'area2',
+    },
+    {
+      type: 'input',
+      message: '3/5 what is one area covered in your application?',
+      name: 'area3',
+    },
+    {
+      type: 'input',
+      message: '4/5 what is one area covered in your application?',
+      name: 'area4',
+    },
+    {
+      type: 'input',
+      message: '5/5 what is one area covered in your application?',
+      name: 'area5',
     },
     {
       type: 'input',
--- Develop/testREADME.md
+++ Develop/testREADME.md
@@ -1,31 +1,19 @@
 
-# readme generator
+# Module-2-Challenge
 
-## 02 Advanced CSS: Portfolio
+## 02 Advance CSS: Portfolio
 
-command line creator of project readme
+This portfolio assignment was a deep dive into CSS styling and layout. Multiple methods of flexbox manipulation were required to accomplish the desireable page layout. the webpage is also
 
-## html, css, js:
+## Areas Covered:
 
-* Webpage header with heading text.
-* Navigation bar with navigation buttons that shade when hovered and jump to respective sections when pressed.
-* Introductory biography for prospective employers to learn more about myself, as well as my capabilities as a developer.
-* Thumbnail clickable previews of previous projects were created to showcase past work and abilities
-* CSS elements were created in order for previous work thumbnails to stack for viewing on narrower screens.
+* Navigation bar with nav buttons that shade on hover and jump to respective sections when clicked.
+* Introductory biography for readers to learn more about myself, as well as learn more about my capabilities as a developer.
+* Thumbnail clickable previews of previous projects were created to showcase past work.
 * Contact methods were added.
-* Used semantic HTML elements.
-* Created comments throughout the CSS style sheet to make future edits and/or navigation easier. 
+* Created comments throughout the CSS style sheet to make navigation and future edits easier.
 
 ## Deployed application: (https://kentshaffer.github.io/module-2-challenge/)
 
-![image screenshot of navigation bar and biography section of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-1.png)
+![Image screenshot of navigationbar and biography section of deployed application on desktop size screen](/Assets/images/module-2-challenge-desktop-1.png)
 
-![image screenshot of project card section of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-2.png)
-
-![image screenshot of contact section and footer of deployed application on desktop size](/Assets/images/module-2-challenge-desktop-3.png) 
-
-![image screenshot of header, nav, and bio of deployed application on smartphone size](/Assets/images/module-2-challenge-mobile-1.png) 
-
-![image screenshot of project card section of deployed application on smartphone size](/Assets/images/module-2-challenge-mobile-2.png) 
-
-![image screenshot of header, bio and top of project card section of deployed application on tablet size](/Assets/images/module-2-challenge-tablet-1.png) 
