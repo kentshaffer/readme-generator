a5fe80b962c93f90978fc0f369df1f0e1400af2c
kent shaffer <kentshaffer@kents-MacBook-Pro.local>
2022-08-16 22:27:59 -0700

    updated project readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,8 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const createReadme = ({ projectTitle, subtitle, description, area1, area2, area3, area4, area5, appLink, screenshot, screenshotDescription }) =>
-//readme skeleton here 
+const createReadme = ({ projectTitle, subtitle, description, area1, area2, area3, area4, area5, appLink, screenshot, screenshotDescription, installation, usage, credits, badges, contDev, tests, license }) =>
+
 `
 # ${projectTitle}
 
@@ -21,7 +20,37 @@ ${description}
 
 ## Deployed application: (${appLink})
 
+
+
 ![${screenshotDescription}](${screenshot})
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Credits
+
+${credits}
+
+## Badges
+
+${badges}
+
+## Continuing Development
+
+${contDev}
+
+## Tests
+
+${tests}
+
+---
+
+© ${license}
 
 `;
 
@@ -44,27 +73,27 @@ inquirer
     },
     {
       type: 'input',
-      message: '1/5 what is one area covered in your application?',
+      message: '1/5 what is an area covered in your application?',
       name: 'area1',
     },
     {
       type: 'input',
-      message: '2/5 what is one area covered in your application?',
+      message: '2/5 what is another area covered in your application?',
       name: 'area2',
     },
     {
       type: 'input',
-      message: '3/5 what is one area covered in your application?',
+      message: '3/5 what is another area covered in your application?',
       name: 'area3',
     },
     {
       type: 'input',
-      message: '4/5 what is one area covered in your application?',
+      message: '4/5 what is another area covered in your application?',
       name: 'area4',
     },
     {
       type: 'input',
-      message: '5/5 what is one area covered in your application?',
+      message: '5/5 what is another area covered in your application?',
       name: 'area5',
     },
     {
@@ -82,31 +111,46 @@ inquirer
       message: 'screenshot description (accessibility)',
       name: 'screenshotDescription',
     },
+    {
+      type: 'input',
+      message: 'Add installation instructions',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'Add usage instructions',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'Add any relevant credits',
+      name: 'credits',
+    },
+    {
+      type: 'input',
+      message: 'Information about earned badges',
+      name: 'badges',
+    },
+    {
+      type: 'input',
+      message: 'Include instructions for future development',
+      name: 'contDev',
+    },
+    {
+      type: 'input',
+      message: 'Show code tests',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'License information',
+      name: 'license',
+    },
   ])
   .then((answers) => {
     const readmeContent = createReadme(answers);
 
-    fs.writeFile('testREADME.md', readmeContent, (err) =>
+    fs.writeFile('newREADME.md', readmeContent, (err) =>
       err ? console.log(err) : console.log('Successfully created your custom README.md!')
     );
   })
-
-
-
-
-
-
-
-
-
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
---
+++ Develop/newREADME.md
@@ -0,0 +1,49 @@
+
+# Some Cool Project Title
+
+## Awesome subtitle 
+
+This is a very detailed description of what my app does. It’s really cool and solves a lot of problems. I worked really hard to make this concept become reality. I really hope you like my project and give it five thumbs up. That’s a quick description of what I built. Thank you.
+
+## Areas Covered:
+
+* Included a user-friendly nav bar with buttons that shade on hover and pop out when clicked
+* Bio section lets the visitor get to know me as the developer
+* Web app navigates between HTML pages flawlessly
+* HTML, CSS, and JavaScript files have been commented for easy navigation and/or future development
+* User inputs save information to local storage
+
+## Deployed application: (https://reallycoolproject.devcreation.llll)
+
+
+
+![Description of image for accessibility or if image doesn’t load for some reason](/Develop/demo-image.png)
+
+## Installation
+
+Generic installation instructions: just press the install button and then wait for the progress bar to load. Installation should only take 3-5 days max
+
+## Usage
+
+Here are the usage instructions in case you can’t figure it out. This is my favorite app, so I hope you get to use it. Step 1. Instructions, step 2. Instructions, step 3. Instructions. All set!
+
+## Credits
+
+I made this all by myself, but if I had help, the names of the contributors would go here. We all worked as a team and navigated many hurdles to make it here today
+
+## Badges
+
+I got like four badges for this project and they are on display here. I hope you think they’re cool.
+
+## Continuing Development
+
+I think it would be great if in the future the app could generate its own calendar on click of the submit button. It would also be really cool if you could set a timer while using the app. If you want to figure out how to add these features, here are the instructions I have so far
+
+## Tests
+
+I’m just learning about testing code, and honestly I’m lost so far, but if this was the readme for a professional project, the tests would go here
+
+---
+
+© Generic licensing information goes here 
+
